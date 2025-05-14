bb2692aa14fb3872576c2f3f0c92b50f3e303fbf
Omshama <omshama328@gmail.com>
2025-05-14 16:31:36 +0530

    Done With Landing Page UI

---
+++ frontend/src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {Toaster} from 'react-hot-toast';
+import Login from './Pages/Auth/Login';
+import SignUp from './Pages/Auth/SignUp';
+import Home from'./Pages/Home/Dashboard';
+import LandingPage from './Pages/InterviewPrep/LandingPage';
+import InterviewPrep from './Pages/InterviewPrep/InterviewPrep';
+const App = () => {
+  return (
+    <div className=''>
+      <Router>
+        <Routes>
+          <Route path='/dashboard' element={<Home/>}/>
+          
+          <Route path='/interview-prep/:sessionId' element={<InterviewPrep/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/signUp' element={<SignUp/>}/>
+          <Route path='/' element={<LandingPage/>}/>
+        </Routes>
+      </Router>
+      <Toaster
+        toastOptions={{
+          className:" ",
+          style:{
+            fontSize:'13px',
+          },
+        }}
+      
+      />
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/utlis/data.js
@@ -0,0 +1,37 @@
+export const APP_FEATURES = [
+    {
+        id:"01",
+        title:"Mock Interviews",
+        description:"Practice mock interviews with AI-powered interviewers to improve your skills and confidence.",
+    },
+    {
+        id:"02",
+        title:"Feedback and Analysis",
+        description:"Receive detailed feedback and analysis on your performance to identify areas for improvement.",
+    },
+    {
+        id:"03",
+        title:"Interview Questions",
+        description:"Access a vast library of interview questions across various domains and industries.",
+    },
+    {
+        id:"04",
+        title:"Personalized Learning",
+        description:"Get personalized learning paths and resources based on your performance and goals.",
+    },
+    {
+        id:"05",
+        title:"Real-time Performance Tracking",
+        description:"Track your performance in real-time during mock interviews and receive instant feedback.",
+    },
+    {
+        id:"06",
+        title:"User-friendly Interface",
+        description:"Enjoy a user-friendly interface that makes it easy to navigate and access all features.",
+    },
+    {
+        id:"07",
+        title:"AI-Powered Insights",
+        description:"Leverage AI-powered insights to enhance your interview preparation and performance.",
+    }
+]
